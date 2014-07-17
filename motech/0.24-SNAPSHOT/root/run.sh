95a10644cee808c4a8b86cc2f9ffe837223d5f06
Rob LaRubbio <rob@larubbio.org>
2014-07-17 13:33:32 -0700

    Add the 0.24 shell

---
+++ motech/0.24-SNAPSHOT/root/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set config with the appropiate host:port of the linked containers
+sed -e 's/COUCH_HOST/'$COUCHDB_1_PORT_5984_TCP_ADDR'/' -i /root/.motech/config/bootstrap.properties
+sed -e 's/COUCH_PORT/'$COUCHDB_1_PORT_5984_TCP_PORT'/' -i /root/.motech/config/bootstrap.properties
+
+sed -e 's/MQ_HOST/'$ACTIVEMQ_1_PORT_61616_TCP_ADDR'/' -i /root/.motech/config/motech-settings.properties
+sed -e 's/MQ_PORT/'$ACTIVEMQ_1_PORT_61616_TCP_PORT'/' -i /root/.motech/config/motech-settings.properties
+
+sed -e 's/DB_TYPE/'$DB_TYPE'/' -i /root/.motech/config/bootstrap.properties
+sed -e 's/DB_HOST/'$DB_1_PORT_3306_TCP_ADDR'/' -i /root/.motech/config/bootstrap.properties
+sed -e 's/DB_PORT/'$DB_1_PORT_3306_TCP_PORT'/' -i /root/.motech/config/bootstrap.properties
+sed -e 's/DB_USER/'$DB_USER'/' -i /root/.motech/config/bootstrap.properties
+sed -e 's/DB_PASSWORD/'$DB_PASSWORD'/' -i /root/.motech/config/bootstrap.properties
+
+# Add the admin user to the database
+sleep 15s
+echo "****************************************************************************************"
+echo "****************************************************************************************"
+echo "****************************************************************************************"
+echo "****************************************************************************************"
+echo "****************************************************************************************"
+curl -X PUT "http://${COUCHDB_1_PORT_5984_TCP_ADDR}:${COUCHDB_1_PORT_5984_TCP_PORT}/${USER}_motech-web-security"
+curl -X POST "http://${COUCHDB_1_PORT_5984_TCP_ADDR}:${COUCHDB_1_PORT_5984_TCP_PORT}/${USER}_motech-web-security" -d @/root/admin-user.json -H 'Content-Type: application/json'
+echo "****************************************************************************************"
+echo "****************************************************************************************"
+echo "****************************************************************************************"
+echo "****************************************************************************************"
+echo "****************************************************************************************"
+
+/opt/tomcat7/bin/catalina.sh run
