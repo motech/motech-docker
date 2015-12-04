3f2d139532765444671263ba790b2c82c706c120
Paweł Gesek <pgesek@soldevelo.com>
2015-12-04 15:22:33 +0100

    Added an image for 0.25.5

---
+++ motech/0.25.5/root/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set config with the appropiate host:port of the linked containers
+sed -e 's/COUCH_HOST/'$COUCHDB_1_PORT_5984_TCP_ADDR'/' -i /root/.motech/config/bootstrap.properties
+sed -e 's/COUCH_PORT/'$COUCHDB_1_PORT_5984_TCP_PORT'/' -i /root/.motech/config/bootstrap.properties
+
+sed -e 's/MQ_HOST/'$ACTIVEMQ_1_PORT_61616_TCP_ADDR'/' -i /root/.motech/config/motech-settings.properties
+sed -e 's/MQ_PORT/'$ACTIVEMQ_1_PORT_61616_TCP_PORT'/' -i /root/.motech/config/motech-settings.properties
+
+sed -e 's/DB_TYPE/'$DB_TYPE'/' -i /root/.motech/config/bootstrap.properties
+sed -e 's/DB_HOST/'$DB_1_PORT_3306_TCP_ADDR'/' -i /root/.motech/config/bootstrap.properties
+sed -e 's/DB_PORT/'$DB_1_PORT_3306_TCP_PORT'/' -i /root/.motech/config/bootstrap.properties
+sed -e 's/DB_USER/'$DB_USER'/' -i /root/.motech/config/bootstrap.properties
+sed -e 's/DB_PASSWORD/'$DB_PASSWORD'/' -i /root/.motech/config/bootstrap.properties
+sed -e 's/DB_DRIVER/'$DB_DRIVER'/' -i /root/.motech/config/bootstrap.properties
+
+/opt/tomcat7/bin/catalina.sh run
