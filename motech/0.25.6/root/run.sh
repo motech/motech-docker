f62be763ffd8bdeb5090968ee7321c990687dfa7
Paweł Gesek <pgesek@soldevelo.com>
2016-01-13 15:21:29 +0100

    Fixed activemq property parsing
    ActiveMQ config now resides in bootstrap, not motech-settings

--- motech/0.25.6/root/run.sh
+++ motech/0.25.6/root/run.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # Set config with the appropiate host:port of the linked containers
-sed -e 's/COUCH_HOST/'$COUCHDB_1_PORT_5984_TCP_ADDR'/' -i /root/.motech/config/bootstrap.properties
-sed -e 's/COUCH_PORT/'$COUCHDB_1_PORT_5984_TCP_PORT'/' -i /root/.motech/config/bootstrap.properties
-
-sed -e 's/MQ_HOST/'$ACTIVEMQ_1_PORT_61616_TCP_ADDR'/' -i /root/.motech/config/motech-settings.properties
-sed -e 's/MQ_PORT/'$ACTIVEMQ_1_PORT_61616_TCP_PORT'/' -i /root/.motech/config/motech-settings.properties
+sed -e 's/MQ_HOST/'$ACTIVEMQ_1_PORT_61616_TCP_ADDR'/' -i /root/.motech/config/bootstrap.properties
+sed -e 's/MQ_PORT/'$ACTIVEMQ_1_PORT_61616_TCP_PORT'/' -i /root/.motech/config/bootstrap.properties
 
 sed -e 's/DB_TYPE/'$DB_TYPE'/' -i /root/.motech/config/bootstrap.properties
 sed -e 's/DB_HOST/'$DB_1_PORT_3306_TCP_ADDR'/' -i /root/.motech/config/bootstrap.properties
