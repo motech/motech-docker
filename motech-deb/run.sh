f75838e0ed8eab002087fdd77e5794db97a7eaae
kmadej <kmadej@soldevelo.com>
2016-04-26 13:13:00 +0200

    MOTECH-2121: The package installs dependencies
    I used ubuntu 15.04 version and now java8 can be easily installed as a package
    dependency. The gdebi installs package dependencies (ActiveMQ, tomcat, etc.).

--- motech-deb/run.sh
+++ motech-deb/run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 # Run activemq
-sudo ln -s /etc/activemq/instances-available/main /etc/activemq/instances-enabled/main
+ln -s /etc/activemq/instances-available/main /etc/activemq/instances-enabled/main
 sed -e 's/<broker /<broker schedulerSupport="true" /' -i /etc/activemq/instances-enabled/main/activemq.xml
 service activemq start
 
 # Run mysql
 service mysql start
-# Set password = 'password' for root
-mysql -uroot -e "UPDATE mysql.user SET Password=PASSWORD('password') WHERE User='root'"
-mysql -uroot -e "FLUSH PRIVILEGES"
 
 # Start motech service
 service motech start
